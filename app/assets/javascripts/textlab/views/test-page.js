d2b111a461f69d1858aa5d11ba56b288236c163f
nicklaiacona <nick@performantsoftware.com>
2016-06-06 19:28:11 -0400

    add opensea dragon for prototyping

--- app/assets/javascripts/textlab/views/test-page.js
+++ app/assets/javascripts/textlab/views/test-page.js
@@ -10,6 +10,19 @@ TextLab.TestPage = Backbone.View.extend({
   render: function() {      
     this.$el.html(this.template({}));  
     $(".textlab-app").html(this.$el);
+    
+    var tileSource = '{"type":"legacy-image-pyramid","levels":[{"url":"https://staging-uploads-juxtaeditions.s3.amazonaws.com/uploads-juxtaeditions.s3.amazonaws.com/uploads/1422303907274-3mlxforsfydjkyb9-35ecdf6c89905871192303efb67f3d16/Page2-3.jpg","width":336,"height":530},{"url":"https://staging-uploads-juxtaeditions.s3.amazonaws.com/uploads-juxtaeditions.s3.amazonaws.com/uploads/1422303907274-3mlxforsfydjkyb9-35ecdf6c89905871192303efb67f3d16/Page2-2.jpg","width":671,"height":1059},{"url":"https://staging-uploads-juxtaeditions.s3.amazonaws.com/uploads-juxtaeditions.s3.amazonaws.com/uploads/1422303907274-3mlxforsfydjkyb9-35ecdf6c89905871192303efb67f3d16/Page2-1.jpg","width":1342,"height":2117},{"url":"https://staging-uploads-juxtaeditions.s3.amazonaws.com/uploads/1422303907274-3mlxforsfydjkyb9-35ecdf6c89905871192303efb67f3d16/Page2.jpg","width":2683,"height":4234}]}';
+    
+		this.seaDragonViewer = OpenSeadragon({
+			id : "openseadragon",
+			prefixUrl : "/openseadragon/",
+			tileSources : tileSource,
+			maxZoomPixelRatio : 5,
+			springStiffness: 20,
+			zoomPerClick: 1.5
+		});
+            
   }
+  
   
 });
