46720ad8e7fda1c398b16895c1f8d1fc27989c5e
nicklaiacona <nick@performantsoftware.com>
2016-06-08 15:54:01 -0400

    some tweaks

--- app/assets/javascripts/textlab/views/leaf-image-viewer.js
+++ app/assets/javascripts/textlab/views/leaf-image-viewer.js
@@ -16,9 +16,7 @@ TextLab.LeafImageViewer = Backbone.View.extend({
 
     this.dragStart = null;
     
-    this.regions = [
-      { left: 100, top: 100, right: 300, botttom: 200 }      
-    ];
+    this.regions = [  ];
     
     this.tileSource = '{"type":"legacy-image-pyramid","levels":[{"url":"https://staging-uploads-juxtaeditions.s3.amazonaws.com/uploads-juxtaeditions.s3.amazonaws.com/uploads/1422303907274-3mlxforsfydjkyb9-35ecdf6c89905871192303efb67f3d16/Page2-3.jpg","width":336,"height":530},{"url":"https://staging-uploads-juxtaeditions.s3.amazonaws.com/uploads-juxtaeditions.s3.amazonaws.com/uploads/1422303907274-3mlxforsfydjkyb9-35ecdf6c89905871192303efb67f3d16/Page2-2.jpg","width":671,"height":1059},{"url":"https://staging-uploads-juxtaeditions.s3.amazonaws.com/uploads-juxtaeditions.s3.amazonaws.com/uploads/1422303907274-3mlxforsfydjkyb9-35ecdf6c89905871192303efb67f3d16/Page2-1.jpg","width":1342,"height":2117},{"url":"https://staging-uploads-juxtaeditions.s3.amazonaws.com/uploads/1422303907274-3mlxforsfydjkyb9-35ecdf6c89905871192303efb67f3d16/Page2.jpg","width":2683,"height":4234}]}';
     
@@ -89,7 +87,7 @@ TextLab.LeafImageViewer = Backbone.View.extend({
     var to = new paper.Point(region.right,region.bottom);
     var rect = new paper.Path.Rectangle(from, to);
     rect.strokeColor = 'red';
-    rect.strokeWidth = 5;
+    rect.strokeWidth = 8;
     rect.onMouseDown = this.onSelectRegion;
     return rect;
   },
