084860895cf859cfc8bfdc5e3cd0b8e75101a255
nicklaiacona <nick@performantsoftware.com>
2016-06-10 11:17:07 -0400

    beginnings of tag toolbar

--- app/assets/javascripts/textlab/views/xml-editor.js
+++ app/assets/javascripts/textlab/views/xml-editor.js
@@ -1,12 +1,51 @@
 TextLab.XMLEditor = Backbone.View.extend({
     
+	template: JST['textlab/templates/xml-editor'],
+    
   id: 'xml-editor',
+  
+  events: {
+    'click .tag-menu-item': 'onClickTagMenuItem'
+  }
             	
 	initialize: function(options) {
+    
+    this.tags = [ 
+      { label: 'add' },   
+      { label: 'addSpan' },   
+      { label: 'anchor' },   
+      { label: 'choice' },   
+      { label: 'corr' },   
+      { label: 'delSpan' },   
+      { label: 'ex' },   
+      { label: 'expan' },   
+      { label: 'handShift' },   
+      { label: 'hi' },   
+      { label: 'l' },   
+      { label: 'lb' },   
+      { label: 'metamark' },   
+      { label: 'rdg' },   
+      { label: 'restore' },   
+      { label: 'sic' },   
+      { label: 'restore' },   
+      { label: 'subst' },   
+      { label: 'restore' },   
+      { label: 'supplied' },   
+      { label: 'unclear' },   
+    ];
+    
   },
+  
+  onClickTagMenuItem: function(e) {
+    if( this.editor ) {
+      
+      // insert tag at caret
+      
+    }
+  }
       
   render: function() {      
-    this.$el.html("<textarea id='codemirror'></textarea>");        
+    this.$el.html(this.template({ tags: this.tags })); 
   },
   
   initEditor: function() {
