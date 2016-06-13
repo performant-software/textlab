302fbf09fac486a215dbc1a1af167aa811bddc53
nicklaiacona <nick@performantsoftware.com>
2016-06-13 15:59:23 -0400

    figuring out structure of tag list

---
+++ app/assets/javascripts/textlab/models/tags.js
@@ -0,0 +1,178 @@
+// TODO 
+
+// TextLab.Tags = [
+//   {
+//     name: 'ab',
+//     attributes: [],
+//     empty: false,
+//     template: null
+//   },
+//   {
+//     name: 'add',
+//     attributes: [
+//       { displayName: 'Place', name: 'place', fieldType: 'vocab', vocab: [] },
+//       { displayName: 'Place', name: 'inline', fieldType: 'vocab', vocab: [] },
+//       { displayName: 'Place', name: 'rendered', fieldType: 'vocab', vocab: [] },
+//       { displayName: 'Place', name: 'renderType', fieldType: 'vocab', vocab: [] },
+//       { displayName: 'Place', name: 'hand', fieldType: 'vocab', vocab: [] },
+//       { displayName: 'Place', name: 'stage', fieldType: 'vocab', vocab: [] },
+//       { displayName: 'Place', name: 'facs', fieldType: 'region' }
+//     ],
+//     template: _.template('place="<%= place %>"> facs="<%= facs %>"')
+//   },
+//   {
+//     label: 'addSpan'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'anchor'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'app'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'choice'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'corr'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'del'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'delSpan'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'div'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'ex'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'expan'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'gap'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'handShift'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'hi'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'l'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'lb'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'metamark'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'milestone'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'p'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'pb'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'rdg'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'restore'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'sic'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'subst'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'supplied'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   },
+//   {
+//     label: 'unclear'
+//     attributes: [],
+//     linkToImage: false,
+//     showOnDropDown: false,
+//   }
+//
+// ];
+//
+//
+//
+//
