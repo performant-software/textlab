72e939d7a1196686069a35680bc6a2b4e0838f35
nicklaiacona <nick@performantsoftware.com>
2016-06-15 14:43:12 -0400

    fix precompile issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( textlab.js )
-Rails.application.config.assets.precompile += %w( textlab.css )
+Rails.application.config.assets.precompile += %w( textlab.scss )
