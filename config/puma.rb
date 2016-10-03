9458b1e58e5f37575935e76c2ef103d3b5b78bf8
nicklaiacona <nick@performantsoftware.com>
2016-10-03 13:17:32 -0400

    try wo workers

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 if ENV['RACK_ENV'] != 'development'
-  workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+  # workers Integer(ENV['WEB_CONCURRENCY'] || 2)
   threads_count = Integer(ENV['MAX_THREADS'] || 5)
   threads threads_count, threads_count
 
