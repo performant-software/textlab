b41ddfc7725c120b89604d07d92d23b55b9dffce
nicklaiacona <nick@performantsoftware.com>
2016-10-13 16:53:53 -0400

    first stab at document sections

--- app/models/document_section.rb
+++ app/models/document_section.rb
@@ -3,6 +3,20 @@ class DocumentSection < ActiveRecord::Base
   belongs_to :document  
   has_one :document_node, dependent: :destroy
   
+  def base_html
+    base_content = ""
+    document_node.child_nodes.order(:position).each { |child_node|  
+      if !child_node.leaf.nil?
+        child_node.leaf.transcriptions.each { |transcription|
+          if transcription.published and !transcription.diplo.nil?
+            base_content << transcription.diplo.html_content
+          end        
+        }        
+      end 
+    }
+    base_content
+  end
+  
   def obj    
     { 
       id: self.id,
