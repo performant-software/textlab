4597a3b64b04091b22e13d68baab7f6f1066e22c
nicklaiacona <nick@performantsoftware.com>
2017-01-31 09:48:37 -0500

    fix formatting of the sequence editor

--- app/assets/javascripts/textlab/views/tabbed-editor.js
+++ app/assets/javascripts/textlab/views/tabbed-editor.js
@@ -140,10 +140,15 @@ TextLab.TabbedEditor = Backbone.View.extend({
   },
   
   resizeActivePanel: function() {    
-    if( this.activeTab && this.parentPanel && this.activeTab.xmlEditor ) {
-      var xmlEditorToolbar = this.$(".xml-editor-toolbar");
-      this.activeTab.xmlEditor.editor.setSize( this.parentPanel.width(), this.parentPanel.height() - xmlEditorToolbar.height() - 100 );
-      this.activeTab.xmlEditor.editor.refresh();
+    if( this.activeTab && this.parentPanel ) {
+      if(  this.activeTab.xmlEditor ) {
+        var xmlEditorToolbar = this.$(".xml-editor-toolbar");
+        this.activeTab.xmlEditor.editor.setSize( this.parentPanel.width(), this.parentPanel.height() - xmlEditorToolbar.height() - 100 );
+        this.activeTab.xmlEditor.editor.refresh();        
+      } else {
+        var sequenceEditor = this.$("#sequence-editor");
+        sequenceEditor.width(this.parentPanel.width());
+      }
     }
   },
   
