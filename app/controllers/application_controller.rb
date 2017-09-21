da74c07bd67ebc4fa0f271087f2907ec1ec36493
nicklaiacona <nick@performantsoftware.com>
2017-09-21 12:52:21 -0400

    require https except for localhost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :redirect_to_https
+
+  def redirect_to_https
+  	redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
+  end
+
 end
