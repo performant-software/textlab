94228a765d2fd3fe605ec263be389fc66fb85941
Andrew Sempere <a@feralresearch.org>
2017-11-08 17:08:01 +0100

    Fix production config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -34,6 +34,9 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+
+  # Do fallback to assets pipeline if a precompiled asset is missed. (ok for dev, not production)
+  config.assets.compile = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
