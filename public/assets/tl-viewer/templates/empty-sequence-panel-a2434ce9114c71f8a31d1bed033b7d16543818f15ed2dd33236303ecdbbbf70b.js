d134f0685c7641b1c4d04a83d0938fc770a3d3cf
Andrew Sempere <a@feralresearch.org>
2017-11-10 23:14:09 +0100

    Revert "Adding CSS to precompiled assets"
    This reverts commit 52d920a1813afc78bd98241a93d0cb74530e878b.

--- public/assets/tl-viewer/templates/empty-sequence-panel-a2434ce9114c71f8a31d1bed033b7d16543818f15ed2dd33236303ecdbbbf70b.js
+++
@@ -1,2 +0,0 @@
-(function() { this.JST || (this.JST = {}); this.JST["tl-viewer/templates/empty-sequence-panel"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h4><a href="#" class=\'back-to-list-link\'>Sequences for Leaf</a> / ',  name ,' by username</h4>\n\n<p>There are no steps for this sequence. Click <a href="#" class=\'back-to-list-link\'>here</a> to return to the sequence list.</p> \n');}return __p.join('');};
-}).call(this);
