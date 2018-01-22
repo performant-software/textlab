aa83c789adb0bee0188126e813edde16c5a02904
Nick Laiacona <nick@performantsoftware.com>
2018-01-22 09:31:32 -0500

    add step number buttons to grid

--- app/assets/javascripts/tl-viewer/views/sequence-panel.js
+++ app/assets/javascripts/tl-viewer/views/sequence-panel.js
@@ -123,9 +123,11 @@ TextLab.SequencePanel = Backbone.View.extend({
 
   render: function() {
     if( this.showGrid ) {
+      var currentStepNumber = (this.currentStep != null) ? this.currentStep.get('step_number') : -1;
       this.$el.html(this.sequenceGridPanelTemplate({
         name: this.model.get('name'),
         owner: this.model.get('owner_name'),
+        currentStep: currentStepNumber,
         steps: this.model.narrativeSteps.toJSON()
       }));
     } else {
