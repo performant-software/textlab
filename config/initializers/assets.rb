9139047662094acd2dea216e6f49e39872ea6211
Tim Dunn <timdunn2@gmail.com>
2020-03-04 00:06:04 +0545

    Remove true from  assets precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,6 @@ Rails.application.config.assets.version = '1.5'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile = true
 Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( textlab.js )
