1adc12c46e3bef29cf7cf2982e747cb5cb1ea9f1
Tim Dunn <timdunn2@gmail.com>
2020-03-03 23:28:08 +0545

    Try to compile assets on staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Force rails to compile all the assets (should never be true on production)
   # Fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
