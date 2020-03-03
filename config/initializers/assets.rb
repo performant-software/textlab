a161295d3bf3a8383a79959f7e52f0f194bebce6
Tim Dunn <timdunn2@gmail.com>
2020-03-03 23:48:43 +0545

    Try to recompile necessary assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.5'
+Rails.application.config.assets.version = '1.6'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -10,6 +10,7 @@ Rails.application.config.assets.version = '1.5'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( search.js )
 
+Rails.application.config.assets.precompile += %w( textlab.js )
 Rails.application.config.assets.precompile += %w( textlab.js )
 Rails.application.config.assets.precompile += %w( textlab.scss )
 
